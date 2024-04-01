ec7e9d522d0e607dc610d0b26d19aab9034afc68
70985_Sudhanshu <sudhanshukulkarni98@gmail.com>
2024-04-01 11:14:20 +0530

    created foodItem,Order,Restaurant,User dto's

---
+++ src/main/java/com/example/order/dto/OrderDTO.java
@@ -0,0 +1,12 @@
+package com.example.order.dto;
+
+import java.util.List;
+
+public class OrderDTO {
+	
+	List<FoodItemsDTO> foodItemList;
+	
+	private Integer userId;
+	
+	private RestaurantDTO restaurantDTO;
+}
